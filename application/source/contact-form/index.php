9bfcfd8b2ab30c02fd186795242627d9115fbd7e
OKozmovskaia <olga.kozmovskaia@gmail.com>
2020-11-25 20:59:36 +0100

    Complete contact-form

--- application/source/contact-form/index.php
+++ application/source/contact-form/index.php
@@ -3,22 +3,73 @@
   $msg = '';
   $msgClass = '';
 
-  // Check for submit
+  // Check the submit is not empty
   if(filter_has_var(INPUT_POST, 'submit')) {
-   
+    // get php mailer for mail()
+    require 'php-mailer/PHPMailerAutoload.php';
+    // create a new php mailer instance
+    $mail = new PHPMailer;
+    // tell PHPMailer to use SMTP
+    $mail->isSMTP();
+
+    // Enable SMTP debugging
+    // 0 = off(for production use)
+    // 1 = client messages
+    // 2 = client and server messages
+    $mail->SMTPDebug = 2;
+    // Ask for HTML-friendly debug output
+    $mail->Debugoutput = 'html';
+    
+    // Set the hostname of the mail server
+    $mail->Host='smtp.gmail.com';
+    // if your network does not support SMTP, over IPv6
+    //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+    $mail->Port=587;
+    //Whether to use SMTP authentication
+    $mail->SMTPAuth=true;
+    //Set the encryption system to use - ssl (deprecated) or tls
+    $mail->SMTPSecure='tls';
+
+    $mail->Username = 'olga.kozmovskaia@gmail.com';
+    $mail->Password = '123456';
+    
     // get form-data
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
-    // check required fields
+    // check that required fields are not empty
     if(!empty($name) && !empty($email) && !empty($message)) {
-      // Check Email
+      // validate Email
       if(filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
         $msg = 'Please use a valid email';
         $msgClass = 'alert-warning';
       } else {
+        // send data from post to email
+        //Set who the message is to be sent to
+        $mail->addAddress('olga.kozmovskaia@gmail.com');
 
+
+        $mail->Subject = 'Contact From'.$name;
+        $mail->Body = '
+        Contact Form
+        Name: '.$name.'
+        Email: '.$email.'
+        Message: '.$message.'
+        ';
+
+        // Email headers
+        $headers = 'MIME-Version: 1.0'.'\r\n';
+        $headers .= 'Content-Type: text/html; charset=UTF-8'.'\r\n';
+        $headers .= 'From: ' .$name.'<'.$email.'>'.'\r\n';
+
+        if(!$mail->send()){
+          $msg = 'Your email was not sent';
+          $msgClass = 'alert-danger';
+        } else {
+            $msg = 'Your email has been sent';
+            $msgClass = 'alert-success';
+        }
       }
       
     } else {
@@ -59,7 +110,7 @@
       <form class="mt-4" method="post" action="">
         <fieldset>
           <div class="form-group">
-            <label for="name">Name</label>
+            <label for="name">Fullname</label>
             <input value = "<?php echo $name; ?>" name="name" type="text" class="form-control" placeholder="Enter name">
           </div>
           <div class="form-group">
@@ -70,7 +121,7 @@
             <label for="message">Message</label>
             <textarea name="message" class="form-control" rows="3"><?php echo $message; ?></textarea>
           </div> 
-            <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+            <button name="submit" type="submit" class="btn btn-primary">Send</button>
         </fieldset>
       </form>
   </div>
